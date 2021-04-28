116ab071c90ce815d974a13e0bced453f699fe3d
jrome-09 <77727574+jrome-09@users.noreply.github.com>
2021-04-28 10:31:11 +0800

    Modifying Navbar

--- navigation.php
+++ navigation.php
@@ -51,13 +51,13 @@
 					</small>
 				</ul>
 			</div> -->
-			<div class="ms-auto">
+			<div class="ms-auto log-sign-container">
 				<small>
-					<a href="#" class="color-white me-1 ms-auto no-text-decor font-500 py-1 px-2 ms-auto">Login</a>
+					<a href="#" class="color-white me-1 ms-auto no-text-decor font-500 py-1 px-2 ms-auto" id="login-btn">Login</a>
 					|
-					<a href="#" class="color-white me-1 ms-auto no-text-decor font-500 py-1 px-2">Sign up</a>
+					<a href="#" class="color-white me-1 ms-1 no-text-decor font-500 py-1 px-2" id="signup-btn">Sign up</a>
 				</small>
-				<a href="#" class="btn btn-outline-primary border-2 color-white px-5 py-2" id="employers-btn"><small>For Employers</small></a>
+				<a href="#" class="btn btn-outline-primary border-2 color-white px-4 py-2 font-500" id="employers-btn"><small>For Employers</small></a>
 			</div>
 		</div>
 	</div>
