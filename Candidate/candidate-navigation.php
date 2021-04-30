63cbe25eeb1818473b578b6c1d801a072c8e1af4
jrome-09 <77727574+jrome-09@users.noreply.github.com>
2021-04-30 14:03:26 +0800

    Creating and Modifying Candidate Navigation

--- Candidate/candidate-navigation.php
+++ Candidate/candidate-navigation.php
@@ -1,3 +1,39 @@
-<head id="candidate-navbar" class="bg-white">
-    
+<head id="candidate-navbar shadow-sm" class="bg-white">
+    <div class="max-width-container py-1">
+        <div class="d-flex align-items-center my-2">
+            <div class="d-flex align-items-center">
+                <div class="image-container bg-light round me-2" style="width: 50px; height: 50px;">
+
+                </div>
+                <h4 class="m-0"><strong><a href="../index.php" class="color-black hover-text-primary" id="web-id">CCIT</a></strong></h4>
+            </div>
+            <nav class="mx-2">
+                <ul class="m-0">
+                    <li class="d-inline-block">
+                        <a href="#" class="fontsize-14 font-500 no-text-decor color-black hover-text-primary mx-2 px-1">Search Jobs</a>
+                    </li>
+                    <li class="d-inline-block">
+                        <a href="#" class="fontsize-14 font-500 no-text-decor color-black hover-text-primary mx-2 px-1">Company Profiles</a>
+                    </li>
+                    <li class="d-inline-block">
+                        <a href="#" class="fontsize-14 font-500 no-text-decor color-black hover-text-primary mx-2 px-1">CCIT Alumni</a>
+                    </li>
+                    <li class="d-inline-block">
+                        <a href="#" class="fontsize-14 font-500 no-text-decor color-black hover-text-primary mx-2 px-1">Forum</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="ms-auto">
+                <div class="btn-group">
+                    <button type="button" class="btn rounded-end color-black fontsize-14 font-500" id="user-name" data-bs-toggle="dropdown">Username <span data-feather="chevron-down" class="color-black"></span></button>
+                    <ul class="dropdown-menu mt-1 p-2">
+                        <li><a href="#" class="dropdown-item rounded py-2 pe-5 color-light fontsize-14 font-400">User Profile</a></li>
+                        <li><a href="#" class="dropdown-item rounded py-2 pe-5 color-light fontsize-14 font-400">Job Applications</a></li>
+                        <li><a href="#" class="dropdown-item rounded py-2 pe-5 color-light fontsize-14 font-400">Logout</a></li>
+                    </ul>
+                </div>
+                <a href="#" type="button" class="btn btn-outline-primary border-2 px-4 py-2 fontsize-14 font-500">For Employers</a>
+            </div>
+        </div>
+    </div>
 </head>
--- Candidate/candidate.php
+++ Candidate/candidate.php
@@ -3,13 +3,15 @@ require "../html/head.html";
 ?>
 
 <body>
-    <link rel="stylesheet" href="candidate-style.scss">
+    <link rel="stylesheet" href="../Candidate/candidate-style.scss">
     <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/style.css">
     <?php
     require "../html/script.html";
+    require "candidate-navigation.php"
     ?>
-    <main>
-        <h1>Hello World!</h1>
+    <main class="bg-primary">
+        <h1 class="color-black">Hello World!</h1>
     </main>
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
