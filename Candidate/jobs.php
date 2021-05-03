1ba27d58517717a86c052aa965c4eb75812e277c
jrome-09 <77727574+jrome-09@users.noreply.github.com>
2021-05-03 11:20:38 +0800

    Creating Job/s Page

---
+++ Candidate/jobs.php
@@ -0,0 +1,29 @@
+<?php
+require "../html/head.html";
+?>
+
+<body>
+    <link rel="stylesheet" href="candidate.css">
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <?php
+    require "../html/script.html";
+    require "candidate-navigation.php"
+    ?>
+    <main>
+    
+    </main>
+    <?php
+    require "../html/footer.html";
+    ?>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+    <script src="../javascript/functions.js"></script>
+    <script src="../javascript/scroll.js"></script>
+    <script src="../javascript/onclick.js"></script>
+    <script>
+        feather.replace();
+    </script>
+</body>
+
+</html>
