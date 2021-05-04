b163fd97d4767f6d7a35b8c8f3265d8ee4827a25
jrome-09 <77727574+jrome-09@users.noreply.github.com>
2021-05-04 13:00:26 +0800

    Creating Job Application Page

---
+++ Candidate/job-applications.php
@@ -0,0 +1,33 @@
+<?php
+require "../html/head.html";
+?>
+
+<body>
+    <div id="spinner-wrapper" class="d-flex justify-content-center align-items-center">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    <link rel="stylesheet" href="candidate.css">
+    <?php
+    require "../html/script.html";
+    require "candidate-navigation.php"
+    ?>
+    <main>
+
+    </main>
+    <?php
+    require "../html/footer.html";
+    ?>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+    <script src="../javascript/functions.js"></script>
+    <script src="../javascript/scroll.js"></script>
+    <script src="../javascript/onclick.js"></script>
+    <script>
+        loadPage();
+        feather.replace();
+    </script>
+</body>
+
+</html>
