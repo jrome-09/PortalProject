455620b8c225b9c959cb5842b5572a462f794b3a
jrome-09 <77727574+jrome-09@users.noreply.github.com>
2021-05-21 13:48:28 +0800

    Creating Alumni Preview Page

---
+++ Alumni/profile-preview.php
@@ -0,0 +1,40 @@
+<?php
+require "../html/head.html";
+?>
+
+<body>
+    <div id="spinner-wrapper" class="d-flex justify-content-center align-items-center">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    <link rel="stylesheet" href="../Candidate/candidate.css">
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="index.css">
+    <?php
+    require "../html/script.html";
+    require "../Candidate/candidate-navigation.php"
+    ?>
+
+    <main>
+        <div class="bg-light margin-top-nav">
+            <div class="max-width-container">
+                
+            </div>
+        </div>
+    </main>
+
+    <?php
+    require "../html/footer.html";
+    ?>
+    <script src="../javascript/functions.js"></script>
+    <script src="../javascript/scroll.js"></script>
+    <script src="../javascript/onclick.js"></script>
+    <script>
+        loadPage();
+        feather.replace();
+    </script>
+</body>
+
+</html>
