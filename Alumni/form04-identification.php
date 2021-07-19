c0d044c8bb11895c5510ed6ae5cb28a4342ede2d
jrome-09 <77727574+jrome-09@users.noreply.github.com>
2021-07-19 10:48:51 +0800

    Creating identification page

---
+++ Alumni/form04-identification.php
@@ -0,0 +1,12 @@
+<?php
+require "alumni-head.html";
+require "../html/script.html";
+?>
+
+<body>
+    <div class="max-width-container">
+        Heyy! Welcome. This is the identification Page.
+    </div>
+</body>
+
+</html>
